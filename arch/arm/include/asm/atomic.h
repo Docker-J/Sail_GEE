4fd395081f12eadb53a109186795c718478362ed
Jake Weinstein <xboxlover360@gmail.com>
2014-09-25 13:51:29 -0500

    Revert "ARM: 7984/1: prefetch: add prefetchw invocations for barriered atomics"
    This reverts commit 4442913201f3c60e162f83cf3326b8307fd2ba52.

--- arch/arm/include/asm/atomic.h
+++ arch/arm/include/asm/atomic.h
@@ -59,7 +59,6 @@ static inline int atomic_add_return(int i, atomic_t *v)
 	int result;
 
 	smp_mb();
-	prefetchw(&v->counter);
 
 	__asm__ __volatile__("@ atomic_add_return\n"
 "1:	ldrex	%0, [%3]\n"
@@ -98,7 +97,6 @@ static inline int atomic_sub_return(int i, atomic_t *v)
 	int result;
 
 	smp_mb();
-	prefetchw(&v->counter);
 
 	__asm__ __volatile__("@ atomic_sub_return\n"
 "1:	ldrex	%0, [%3]\n"
@@ -120,7 +118,6 @@ static inline int atomic_cmpxchg(atomic_t *ptr, int old, int new)
 	unsigned long oldval, res;
 
 	smp_mb();
-	prefetchw(&ptr->counter);
 
 	do {
 		__asm__ __volatile__("@ atomic_cmpxchg\n"
@@ -273,7 +270,6 @@ static inline u64 atomic64_add_return(u64 i, atomic64_t *v)
 	unsigned long tmp;
 
 	smp_mb();
-	prefetchw(&v->counter);
 
 	__asm__ __volatile__("@ atomic64_add_return\n"
 "1:	ldrexd	%0, %H0, [%3]\n"
@@ -314,7 +310,6 @@ static inline u64 atomic64_sub_return(u64 i, atomic64_t *v)
 	unsigned long tmp;
 
 	smp_mb();
-	prefetchw(&v->counter);
 
 	__asm__ __volatile__("@ atomic64_sub_return\n"
 "1:	ldrexd	%0, %H0, [%3]\n"
@@ -338,7 +333,6 @@ static inline u64 atomic64_cmpxchg(atomic64_t *ptr, u64 old, u64 new)
 	unsigned long res;
 
 	smp_mb();
-	prefetchw(&ptr->counter);
 
 	do {
 		__asm__ __volatile__("@ atomic64_cmpxchg\n"
@@ -363,7 +357,6 @@ static inline u64 atomic64_xchg(atomic64_t *ptr, u64 new)
 	unsigned long tmp;
 
 	smp_mb();
-	prefetchw(&ptr->counter);
 
 	__asm__ __volatile__("@ atomic64_xchg\n"
 "1:	ldrexd	%0, %H0, [%3]\n"
@@ -385,7 +378,6 @@ static inline u64 atomic64_dec_if_positive(atomic64_t *v)
 	unsigned long tmp;
 
 	smp_mb();
-	prefetchw(&v->counter);
 
 	__asm__ __volatile__("@ atomic64_dec_if_positive\n"
 "1:	ldrexd	%0, %H0, [%3]\n"
@@ -413,7 +405,6 @@ static inline int atomic64_add_unless(atomic64_t *v, u64 a, u64 u)
 	int ret = 1;
 
 	smp_mb();
-	prefetchw(&v->counter);
 
 	__asm__ __volatile__("@ atomic64_add_unless\n"
 "1:	ldrexd	%0, %H0, [%4]\n"
